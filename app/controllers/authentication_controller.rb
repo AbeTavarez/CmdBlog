a85080f6727ff211f3fe4efd7f8462e41b25487e
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-10 19:48:30 -0400

    auth action created

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,26 @@
 class AuthenticationController < ApplicationController
+  before_action :authorize_request, except: :login
+
+  # POST /auth/login
+  def login
+    @user = User.find_by_username(login_params[:username])
+    if @user.authenticate(login_params[:password]) #authenticate method provided by Bcrypt and 'has_secure_password'
+      token = encode(user_id: @user.id, username: @user.username)
+      render json: { user: @user, token: token }, status: :ok
+    else
+      render json: { errors: 'unauthorized' }, status: :unauthorized
+    end
+  end
+  
+  # GET /auth/verify
+  def verify
+    render json: @current_user, status: :ok
+  end
+
+
+  private
+
+  def login_params
+    params.require(:auth).permit(:username, :password)
+  end
 end
