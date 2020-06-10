71c45639c20a7ab29f053c14db4873faef97dade
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-10 19:56:04 -0400

    created auth_request to auth any login attempt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,20 @@ class ApplicationController < ActionController::API
     decoded = JWT.decode(token, SECRET_KEY)[0]
     HashWithIndifferentAccess.new decoded
   end
+
+  #:::::::::::::::::::::::::::::::::::::::::::::::::::::
+  #this method will be inherit by all other controllers
+  #:::::::::::::::::::::::::::::::::::::::::::::::::::::
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
