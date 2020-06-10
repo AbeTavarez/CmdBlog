71f460ed230d66ed07e5a7f0ed12e578f12963b8
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-09 23:47:24 -0400

    Created some default data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Article.destroy_all
+User.destroy_all
+
+#create default user
+@user = user.create!({username: "Abe", email: "abe@gmail.com",password: "123456"})
+
+p "#{User.count} user(s) created"
+
+#Create some Categories for our category table
+@programming = Category.create!("Programming")
+@hacking = Category.create!("Hacking")
+@cybersecurity = Category.create!("Cybersecurity")
+
+Article.create!({title: "HackingWithPython", description: "Write scripts in python", topic: "coding", user_id: @user.id, category_id: @hacking.id})
+
+p "#{Article.count} article(s) created"
