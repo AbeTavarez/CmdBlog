668e12d9ba9d32cab08460cd84e33431c7a3b3d2
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-09 21:29:13 -0400

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }
+
+  has_many :articles, dependent: :destroy
 end
--- db/migrate/20200610004459_create_articles.rb
+++ db/migrate/20200610004459_create_articles.rb
@@ -4,6 +4,8 @@ class CreateArticles < ActiveRecord::Migration[6.0]
       t.string :title
       t.text :description
       t.string :category
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
