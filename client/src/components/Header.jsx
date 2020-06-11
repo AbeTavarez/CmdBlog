e20ea8c4ad541ed9f7c49caff71089113021dbce
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-11 16:28:45 -0400

    setting up react-router-dom

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { Link, NavLink } from "react-router-dom";
+
 export default class Header extends Component {
   render() {
     return <div>This is the header</div>;
