0657de9c8435e6c0cce8b95733cd45b7cfb8b33c
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-12 16:12:22 -0400

    test

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseUrl = "http://locaalhost:3000";
+process.env.NODE_ENV === "production"
+  ? "https://cmdblog-api.herokuapp.com/"
+  : "http://localhost:3000";
 const api = axios.create({
   baseUrl: baseUrl,
 });
